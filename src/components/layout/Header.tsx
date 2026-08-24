6ec0e79eddd66a775f3bcb57b53755a34383356c
taiza-langa <taizalanga123@gmail.com>
2026-08-24 20:32:24 -0300

    Ajuste no header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import ItemNav from "./ItemNav";
 
 export default function Header() {
     return (
-        <div className="flex border-b justify-between px-8 py-4">
+        <div className="mx-auto flex max-w-7xl justify-between px-6 py-4 sm:px-10 lg:px-1">
             <div className="flex gap-2 items-center">
                 <img className="max-h-10" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJdAo2rZzw08S0RV5VevGLR-PRuehXvErkHmJBVhWnrw&s=10'></img>
                 <h2>Casa e Madeira</h2>
@@ -19,7 +19,7 @@ export default function Header() {
                 </Nav>
             </div>
             <div className="flex items-center gap-2">
-                <User size={20}className="hover:text-accent"></User>
+                <User size={20} className="hover:text-accent"></User>
                 <Button title="Carrinho" color="primary" icon={ShoppingCart}></Button>
             </div>
         </div>
