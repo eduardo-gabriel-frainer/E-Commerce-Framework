f8929acd455900118c0a29ea32535f9be256cc93
taiza-langa <taizalanga123@gmail.com>
2026-08-24 20:59:29 -0300

    Ajuste page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/header/Header";
 import GridCards from "@/components/layout/main/GridCards";
 import ProductPresentation from "@/components/layout/main/ProductPresentation";
 
